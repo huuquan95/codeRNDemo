fa4425bc71554a1ef110c6c9d31e5fb8a8d66ca6
Đinh Hữu Quân <huuquan95@gmail.com>
2018-07-20 10:11:29 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
-
+//abc
 export default class App extends Component{
     render(){
         let textToDisplay='\r\rThis is App.js file.'
